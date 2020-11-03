dbbc0cba2688ea7892ae836aa12a3b04e5c4d0c4
MarkoBox <markoboki@hotmail.com>
2020-11-03 01:18:49 +0100

    Initial commit, Models and Managers complete.

---
+++ market_data/managers.py
@@ -0,0 +1,64 @@
+from django.db.models import QuerySet, Manager
+import requests
+from django.utils import timezone
+
+
+class ApiCallHistoryQuerySet(QuerySet):
+    pass
+
+
+class ApiCallHistoryManager(Manager):
+    def get_queryset(self):
+        return ApiCallHistoryQuerySet(self.model, using=self._db)
+
+    def import_new_market_data(self):
+        url = "http://api.bitcoincharts.com/v1/markets.json"
+        update_time = timezone.now()
+
+        data = requests.get(url=url).json()
+        objs = [self.model(returned_json=line, update_time=update_time) for line in data]
+        self.model.objects.bulk_create(objs)
+        print(update_time)
+        return self.model.objects.filter(update_time=update_time), update_time
+
+
+class CoinQuerySet(QuerySet):
+    pass
+
+
+class CoinManager(Manager):
+    def get_queryset(self):
+        return ApiCallHistoryQuerySet(self.model, using=self._db)
+
+    def update_new_coins(self):
+        inserted_rows = self.model.objects.raw("""INSERT INTO market_data_coin(ticker_symbol)
+        SELECT DISTINCT returned_json ->> 'symbol' from market_data_apicallhistory
+        LEFT JOIN market_data_coin
+            ON  returned_json ->> 'symbol' = market_data_coin.ticker_symbol
+        WHERE market_data_coin.ticker_symbol IS NULL;""")
+        return inserted_rows
+
+
+class TradeDataQuerySet(QuerySet):
+    pass
+
+
+class TradeDataManager(Manager):
+    def get_queryset(self):
+        return TradeDataQuerySet(self.model, using=self._db)
+
+    def import_data_updated_at(self, update_time):
+        inserter_rows = self.model.objects.raw(raw_query="""WITH new_data AS (
+        SELECT returned_json, update_time, market_data_coin.id AS coin_id
+        FROM market_data_apicallhistory
+                LEFT JOIN market_data_coin
+                        ON market_data_apicallhistory.returned_json ->> 'symbol' = market_data_coin.ticker_symbol
+        WHERE update_time = %s
+        )
+        INSERT
+            INTO market_data_tradedata (currency_volume, weighted_price, update_time, coin_id)
+            SELECT CAST(new_data.returned_json ->> 'currency_volume' AS DECIMAL),
+                   CAST(new_data.returned_json ->> 'weighted_price' AS DECIMAL),
+                   new_data.update_time,
+                   new_data.coin_id FROM new_data;""", params=[update_time])
+        return inserter_rows
---
+++ market_data/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from .managers import ApiCallHistoryManager, CoinManager, TradeDataManager
+
+
+class ApiCallHistory(models.Model):
+    returned_json = models.JSONField()
+    update_time = models.DateTimeField()
+
+    objects = ApiCallHistoryManager()
+
+
+class Coin(models.Model):
+    ticker_symbol = models.TextField(unique=True, max_length=20)
+
+    objects = CoinManager()
+
+
+class TradeData(models.Model):
+    currency_volume = models.DecimalField(max_digits=25, decimal_places=2, null=True, blank=True,
+                                          verbose_name='Currency volume') # TODO: 4 places for consistency
+    weighted_price = models.DecimalField(max_digits=25, decimal_places=4, null=True, blank=True,
+                                         verbose_name='Weighted Price')
+    update_time = models.DateTimeField()
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE, null=True, blank=True)
+
+    objects = TradeDataManager()
+
