51ddf1149b1c2d46696eb511acac0b4796ecff41
MarkoBox <markoboki@hotmail.com>
2020-11-04 23:55:18 +0100

    Latest data paginator complete

--- market_data/api/serializers.py
+++ market_data/api/serializers.py
@@ -3,15 +3,16 @@ from rest_framework import serializers
 from ..models import TradeData, Coin
 
 
-class TradeDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TradeData
-        fields = ['currency_volume', 'weighted_price', 'update_time']
-
-
 class CoinSerializer(serializers.ModelSerializer):
-    trade_data = TradeDataSerializer(many=True)
 
     class Meta:
         model = Coin
-        fields = ['ticker_symbol', 'trade_data']
+        fields = ['ticker_symbol']
+
+
+class TradeDataSerializer(serializers.ModelSerializer):
+    coin = CoinSerializer()
+
+    class Meta:
+        model = TradeData
+        fields = ['currency_volume', 'weighted_price', 'update_time', 'coin']
--- market_data/api/views.py
+++ market_data/api/views.py
@@ -1,16 +1,22 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import generics
 from .serializers import TradeDataSerializer, CoinSerializer
 from ..models import TradeData, Coin
 
 
+class TopPaginator(PageNumberPagination):
+    page_size = 20
+    page_query_param = 'page_size'
+    max_page_size = 20
+
+
 class LatestTradeDataView(generics.ListAPIView):
     serializer_class = TradeDataSerializer
+    pagination_class = TopPaginator
 
     def get_queryset(self):
         latest_update_time = TradeData.objects.latest('update_time').update_time
-        return TradeData.objects.filter(update_time__exact=latest_update_time)
+        return TradeData.objects.filter(update_time__exact=latest_update_time).order_by('-currency_volume')
 
 
 class CoinView(generics.ListAPIView):
