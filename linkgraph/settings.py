9d535028a33982bb10b3ed452ec6188711857b93
MarkoBox <markoboki@hotmail.com>
2020-11-18 12:10:37 +0100

    Added celery with basic task

--- linkgraph/settings.py
+++ linkgraph/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,3 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# celery settings
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_BEAT_SCHEDULE = {
+    'hello': {
+        'task': 'market_data.tasks.hello',
+        'schedule': crontab()  # execute every minute
+    }
+}
--- market_data/tasks.py
+++ market_data/tasks.py
@@ -1,7 +1,13 @@
 from market_data.models import ApiCallHistory, Coin, TradeData
+from celery import shared_task
 
 
 def update_market_data():
     update_time = ApiCallHistory.objects.import_new_market_data()[1]
     Coin.objects.update_new_coins()
     TradeData.objects.import_data_updated_at(update_time=update_time)
+
+
+@shared_task
+def hello():
+    print("Hello there!")
